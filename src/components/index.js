048bb69983726098d13ee960f13e4139433c51db
Andrew Motko <andrew.motko@gmail.com>
2024-03-05 02:27:46 +0200

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from 'components/App/App';
+export * from 'components/Section/Section';
+export * from 'components/ContactForm/ContactForm';
+export * from 'components/ContactList/ContactList';
+export * from 'components/Filter/Filter';
